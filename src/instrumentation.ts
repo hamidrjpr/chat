e76c2a737b43332a35ce799655b9f831aebedb98
hamidrjpr <hamid.hr1380@gmail.com>
2025-05-30 11:47:31 +0330

    feat: add config files to project

---
+++ src/instrumentation.ts
@@ -0,0 +1,16 @@
+import { Instrumentation } from "next";
+
+export function register() {
+  if (process.env.NODE_ENV === "development") {
+    console.log("alireza");
+  }
+  console.log("hamid");
+}
+
+export const onRequestError: Instrumentation.onRequestError = async (
+  err,
+  request,
+  context
+): Promise<void> => {
+  console.log(err, request, context);
+};
